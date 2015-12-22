a10dab3bade0dd8a05aa0850ed8961131a84960e
Federico <federico.fiorini29@gmail.com>
2015-12-22 18:10:12 +0100

    minor changes in google tokens

--- src/ucoach/data/endpoint/GoogleTokensPublisher.java
+++ src/ucoach/data/endpoint/GoogleTokensPublisher.java
@@ -8,35 +8,35 @@ import javax.xml.ws.Endpoint;
 
 public class GoogleTokensPublisher {
 
-    public static String BASE_URL = "/ws/google-tokens";
+  public static String BASE_URL = "/ws/google-tokens";
 
-    /**
-     * Build endpoint url
-     * @return
-     * @throws UnknownHostException 
-     */
-    public static String getEndpointURL() throws UnknownHostException {
+  /**
+   * Build endpoint url
+   * @return
+   * @throws UnknownHostException 
+   */
+  public static String getEndpointURL() throws UnknownHostException {
 
-    	String port_value = "6900";
-        if (String.valueOf(System.getenv("PORT")) != "null"){
-            port_value = String.valueOf(System.getenv("PORT"));
-        }
-        String port = ":" + port_value;
-        
-        String hostname = InetAddress.getLocalHost().getHostAddress();
-        if (hostname.equals("127.0.0.1"))
-        {
-            hostname = "localhost";
-        }
+  	String port_value = "6900";
+    if (String.valueOf(System.getenv("PORT")) != "null"){
+        port_value = String.valueOf(System.getenv("PORT"));
+    }
+    String port = ":" + port_value;
 
-        return "http://" + hostname + port + BASE_URL;        
+    String hostname = InetAddress.getLocalHost().getHostAddress();
+    if (hostname.equals("127.0.0.1"))
+    {
+        hostname = "localhost";
     }
 
-    public static void main(String[] args) throws UnknownHostException {
-    	
-        String endpointUrl = getEndpointURL();
-        System.out.println("Starting GoogleTokens service");
-        System.out.println("--> Published at " + endpointUrl);
-        Endpoint.publish(endpointUrl, new GoogleTokensImpl());
-    }
+    return "http://" + hostname + port + BASE_URL;
+  }
+
+  public static void main(String[] args) throws UnknownHostException {
+
+    String endpointUrl = getEndpointURL();
+    System.out.println("Starting GoogleTokens service");
+    System.out.println("--> Published at " + endpointUrl);
+    Endpoint.publish(endpointUrl, new GoogleTokensImpl());
+  }
 }
