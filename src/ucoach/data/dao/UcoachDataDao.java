ab6cc7e19bdccbc159b62a9990d512165a0547ca
Ana Daniel <anadanield@gmail.com>
2016-02-06 22:31:57 +0100

    Add heroku clearDB config

--- src/ucoach/data/dao/UcoachDataDao.java
+++ src/ucoach/data/dao/UcoachDataDao.java
@@ -5,9 +5,38 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import java.util.StringTokenizer;
+import java.util.Map;
+import java.util.HashMap;
+
 public enum UcoachDataDao {
   instance;
   private EntityManagerFactory emf;
+
+  private Map<String, String> getDBProperties(){
+    String databaseUrl = System.getenv("CLEARDB_DATABASE_URL");
+    Map<String, String> properties = new HashMap<String, String>();
+
+    if( databaseUrl != null ) {
+      StringTokenizer st = new StringTokenizer(databaseUrl, ":@/");
+      String dbVendor = st.nextToken();
+      String username = st.nextToken();
+      String password = st.nextToken();
+      String host = st.nextToken();
+      String databaseName = st.nextToken();
+      String jdbcUrl = String.format("jdbc:%s://%s/%s", dbVendor, host, databaseName);
+      properties.put("hibernate.connection.url", jdbcUrl);
+      properties.put("hibernate.connection.username", username);
+      properties.put("hibernate.connection.password", password);
+
+    } else {
+      properties.put("hibernate.connection.url", "jdbc:mysql://127.0.0.1:3306/ucoach_data_service");
+      properties.put("hibernate.connection.username", "root");
+      properties.put("hibernate.connection.password", "root");
+    }
+
+    return properties;
+  }
 
   private UcoachDataDao() {
     if (emf!=null) {
@@ -15,7 +44,7 @@ public enum UcoachDataDao {
     }
     
     try {
-      emf = Persistence.createEntityManagerFactory("ucoach-data-jpa");
+      emf = Persistence.createEntityManagerFactory("ucoach-data-jpa", getDBProperties());
     } catch (Exception e) {
       e.printStackTrace();
     }
